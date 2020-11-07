285dcdae60a65a4e285fab64427f230d5c324338
spder007 <67345799+spder007@users.noreply.github.com>
2020-11-07 20:48:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,35 @@
+const express = require("express");
+const { Client, RichEmbed } = require("discord.js");
+const app = express();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(Date.now() + "hostemprator");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const data = new Map();
 
+client.login(process.env.token);
+
+client.on("ready", () => {
+  console.log(); ///By 010101
+  let statuses = [
+    `BY 010101`,
+    `PARTNER BOT`,
+    `ئەگەر بۆ ڕێکلام هاتووی بۆ منی بێرە`
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
